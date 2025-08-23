0b7ecb2b1566b55316deb45bfebd75b9b9fbc5e6
Maja Frydrychowicz <mjzffr@gmail.com>
2025-08-23 12:29:52 -0400

    Update prettier and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,45 @@
 import globals from 'globals';
 import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   // files to ignore entirely
   {
     ignores: [
-      '**/node_modules/*', 
-      '**/dist/*', 
-      '**/build/*', 
+      '**/node_modules/*',
+      '**/dist/*',
+      '**/build/*',
       '**/webpack.config.{js,cjs}',
-      'eslint.config.mjs',
-    ],
+      'eslint.config.mjs'
+    ]
   },
   //use recommended rules as a base for all files and then override
   js.configs.recommended,
   //override rules for all files
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     languageOptions: {
       globals: {
         ...globals.browser
       },
-      sourceType: 'script',
+      sourceType: 'script'
     },
     rules: {
-    ////////// Possible Errors //////////
-      'no-console': ['warn', { 'allow': ['error'] }],
+      ////////// Possible Errors //////////
+      'no-console': ['warn', {allow: ['error']}],
       'one-var': ['warn', 'never'],
       'no-undef': 'warn',
       'prefer-const': 'warn',
       'no-extra-parens': 'warn',
       'block-scoped-var': 'warn',
-      'curly': ['warn', 'multi-line'],
+      curly: ['warn', 'multi-line'],
       'no-await-in-loop': 'warn',
       'no-cond-assign': ['error', 'always'],
       'no-debugger': 'warn',
       'default-case': 'warn',
-      'dot-location': ['warn', 'object'],
-      'eqeqeq': 'warn',
+      eqeqeq: 'warn',
       'no-alert': 'warn',
       'no-eq-null': 'warn',
       'no-eval': 'warn',
@@ -45,26 +48,33 @@ export default [
       'no-loop-func': 'warn',
       'no-multi-str': 'warn',
       'no-self-compare': 'warn',
-      'strict': ['warn', 'global'],
+      strict: ['warn', 'global'],
       'no-lonely-if': 'warn',
       ////////// Style for Graded Submissions //////////
-      'camelcase': 'error',
+      camelcase: 'error',
       'no-inline-comments': 'error',
-      // other style expectations are handled by Prettier
-      // 'array-bracket-spacing': ['error', 'never'], // deprecated, handled by Prettier
-      // 'array-bracket-newline': ['error', 'consistent'], // deprecated, handled by Prettier
-      // 'indent': ['error', 2], // deprecated, handled by Prettier
-      // 'comma-spacing': ['error', { 'before': false, 'after': true }], // deprecated, handled by Prettier
-      // 'comma-style': ['error', 'last'], // deprecated, handled by Prettier
-      // 'brace-style': ['error'], // deprecated, handled by Prettier
-      // 'max-len': ['error', 100], // deprecated, handled by Prettier
-      // 'no-tabs': 'error', // deprecated, handled by Prettier
-      // 'quotes': ['error', 'single', {'allowTemplateLiterals': true}], // deprecated, handled by Prettier
-      // 'jsx-quotes': ['error', 'prefer-double'], // deprecated, handled by Prettier
-      // 'space-infix-ops': 'error', // deprecated, handled by Prettier
-      // 'space-unary-ops': 'error', // deprecated, handled by Prettier
-      // 'semi': 'error', // deprecated, handled by Prettier
-      // 'semi-spacing': 'error' // deprecated, handled by Prettier
+      // To fix style formatting, run
+      // npx prettier --write .
+      // and/or
+      // npx eslint --fix .
+      '@stylistic/array-bracket-spacing': ['error', 'never'],
+      '@stylistic/array-bracket-newline': ['error', 'consistent'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/comma-spacing': ['error', {before: false, after: true}],
+      '@stylistic/comma-style': ['error', 'last'],
+      '@stylistic/brace-style': ['error'],
+      '@stylistic/max-len': ['error', 100],
+      '@stylistic/no-tabs': 'error',
+      '@stylistic/quotes': [
+        'error',
+        'single',
+        {allowTemplateLiterals: 'always'}
+      ],
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/space-infix-ops': 'error',
+      '@stylistic/space-unary-ops': 'error',
+      '@stylistic/semi': 'error',
+      '@stylistic/semi-spacing': 'error'
     }
-  },
+  }
 ];
