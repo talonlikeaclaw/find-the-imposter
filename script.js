116f8feb5b6022577f90d06a9858a468612fa205
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-08-26 15:12:42 -0400

    Add imposter and click indicator classes to images

--- script.js
+++ script.js
@@ -25,18 +25,26 @@ function addImageBatchImages() {
   const firstDogImage = document.createElement('img');
   firstDogImage.src = 'assets/puppy-01.jpg';
   firstDogImage.classList.add('batch-item');
+  firstDogImage.classList.add('nonimposter');
+  firstDogImage.classList.add('notClicked');
 
   const secondDogImage = document.createElement('img');
   secondDogImage.src = 'assets/puppy-02.jpg';
   secondDogImage.classList.add('batch-item');
+  secondDogImage.classList.add('nonimposter');
+  secondDogImage.classList.add('notClicked');
 
   const thirdDogImage = document.createElement('img');
   thirdDogImage.src = 'assets/puppy-03.jpg';
   thirdDogImage.classList.add('batch-item');
+  thirdDogImage.classList.add('nonimposter');
+  thirdDogImage.classList.add('notClicked');
 
   const imposterBearImage = document.createElement('img');
   imposterBearImage.src = 'assets/bear-01.jpg';
   imposterBearImage.classList.add('batch-item');
+  imposterBearImage.classList.add('imposter');
+  imposterBearImage.classList.add('notClicked');
 
   imageBatch.appendChild(firstDogImage);
   imageBatch.appendChild(secondDogImage);
